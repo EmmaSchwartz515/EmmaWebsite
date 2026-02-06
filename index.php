8e70b667413a41b35e19a9fb36f8cfe5e765b865
EmmaSchwartz <emma.schwartz515@myci.csuci.edu>
2026-02-06 15:59:15 -0800

    First PHP

--- index.php
+++ index.php
@@ -1,1 +1,25 @@
-echo phpversion();
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title></title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="index.css">
+    </head>
+    <body>
+        <div classname="about-sect">
+            <p classname="job-desc">[Computer Scientist]</p>
+            <h1 classname="title">Emma Schwartz</h1>
+            <p classname="about-me">Working as a <em>Web Service Analyst</em> for <strong>CSU Channel Islands</strong></p>
+        </div>
+
+        <?php
+            echo "hello world!";
+        ?>
+
+        <h2></h2>
+        <script src="index.js" async defer></script>
+    </body>
+</html>
