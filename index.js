208aa82059fe2de5c2c91cddf5d81363d1048b09
EmmaSchwartz <emma.schwartz515@myci.csuci.edu>
2026-02-09 16:38:16 -0800

    debug?

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 function toggleSection(id) {
-  document.getElementById(id).classList.toggle("show");
+    document.getElementById(id).classList.toggle("show");
+    alert("WOW YOU PRESSED THE BUTTON");
 }
