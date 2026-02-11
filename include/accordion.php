10b1f23250bb90452a82e6046a536e3238e27a0c
EmmaSchwartz <emma.schwartz515@myci.csuci.edu>
2026-02-11 11:26:55 -0800

    Update accordion.php

--- include/accordion.php
+++ include/accordion.php
@@ -2,6 +2,6 @@
     <?php
         $_GET["name"] = "Erm";
         $_GET["content"] = "<div>HELLO SCOOBERT</div>";
-        include("accordion-item");
+        include("accordion-item.php");
     ?>
 </ul>
