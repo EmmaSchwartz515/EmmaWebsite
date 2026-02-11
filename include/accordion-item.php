8503db8ae41517c6fcf7cde5891533a9f96cfa93
EmmaSchwartz <emma.schwartz515@myci.csuci.edu>
2026-02-11 11:26:14 -0800

    accordion menu attempt at automating

---
+++ include/accordion-item.php
@@ -0,0 +1,17 @@
+<li>
+    <div class="sect-dropdown">
+        <button onclick="toggleSection()" class="sect-dpdown-btn">
+            <?php
+                echo $_GET["name"];
+            ?>
+            <div class="collapse">
+                <i class="fa-solid fa-plus" aria-live="off"></i>
+            </div>
+        </button>
+    </div>
+    <div class="sect">
+        <?php
+            echo $_GET["content"];
+        ?>
+    </div>
+</li>
