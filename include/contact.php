9441d57958120293cef6f685d305ed31a756f53f
EmmaSchwartz <emma.schwartz515@myci.csuci.edu>
2026-02-11 11:07:06 -0800

    Move Plant Image to Front and Attempt to put Acceptance at Bottom of Form

--- include/contact.php
+++ include/contact.php
@@ -9,17 +9,18 @@
     <br><textarea rows="5" name="message" cols="30"></textarea><br>
     <input type="submit" name="submit" value="Submit">
 </form>
+<p>
+    <?php
+        if (isset($_POST['submit'])){
+            $to = "emma.schwartz515@myci.csuci.edu";
+            $from = $_POST['email'];
+            $first_name = $_POST['first_name'];
+            $last_name = $_POST['last_name'];
+            $subject = "Form submission";
+            $message = $first_name . " " . $last_name . " (" . $from . ")wrote the following:" . "\n\n" . $_POST['message'];
 
-<?php
-    if (isset($_POST['submit'])){
-        $to = "emma.schwartz515@myci.csuci.edu";
-        $from = $_POST['email'];
-        $first_name = $_POST['first_name'];
-        $last_name = $_POST['last_name'];
-        $subject = "Form submission";
-        $message = $first_name . " " . $last_name . " (" . $from . ")wrote the following:" . "\n\n" . $_POST['message'];
-
-        mail($to, $subject, $message);
-        echo("Mail Sent. Thank you " . $first_name . ", we will contact you shortly.");
-    }
-?>
+            mail($to, $subject, $message);
+            echo("Mail Sent. Thank you " . $first_name . ", we will contact you shortly.");
+        }
+    ?>
+</p>
