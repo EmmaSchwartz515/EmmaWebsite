8221673fd28e4b2f7c0220e4ee4cee514f1dfaa5
EmmaSchwartz <emma.schwartz515@myci.csuci.edu>
2026-02-11 11:02:57 -0800

    Move Confirmation message to bottom!

--- include/contact.php
+++ include/contact.php
@@ -1,18 +1,3 @@
-<?php
-    if (isset($_POST['submit'])){
-        $to = "emma.schwartz515@myci.csuci.edu";
-        $from = $_POST['email'];
-        $first_name = $_POST['first_name'];
-        $last_name = $_POST['last_name'];
-        $subject = "Form submission";
-        $message = $first_name . " " . $last_name . " wrote the following:" . "\n\n" . $_POST['message'];
-
-        $headers = "From:" . $from;
-        mail($to,$subject,$message,$headers);
-        echo "Mail Sent. Thank you " . $first_name . ", we will contact you shortly.";
-    }
-?>
-
 <form action="" method="post">
     <label>First Name: </label>
     <input type="text" name="first_name"><br>
@@ -24,3 +9,17 @@
     <br><textarea rows="5" name="message" cols="30"></textarea><br>
     <input type="submit" name="submit" value="Submit">
 </form>
+
+<?php
+    if (isset($_POST['submit'])){
+        $to = "emma.schwartz515@myci.csuci.edu";
+        $from = $_POST['email'];
+        $first_name = $_POST['first_name'];
+        $last_name = $_POST['last_name'];
+        $subject = "Form submission";
+        $message = $first_name . " " . $last_name . " (" . $from . ")wrote the following:" . "\n\n" . $_POST['message'];
+
+        mail($to, $subject, $message);
+        echo("Mail Sent. Thank you " . $first_name . ", we will contact you shortly.");
+    }
+?>
