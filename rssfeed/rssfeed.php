71985e2ed237c8afbbe04beb3e1c7c333eacad91
EmmaSchwartz <emma.schwartz515@myci.csuci.edu>
2026-02-24 11:16:26 -0800

    Revert "Remove form resubmission attempt #2"
    This reverts commit 7050cb0997bd13aa7c07d55f2bdd7d283e553e64.

--- rssfeed/rssfeed.php
+++ rssfeed/rssfeed.php
@@ -13,16 +13,14 @@
             <?php
                 $maxItems = 5;
 
-                if ( $_SERVER['REQUEST_METHOD'] === 'POST' ) {
-                    if (!isset($_POST["showless"])) {
-                        if (isset($_POST["showmore"])) {
-                            $maxItems = -1;
-                        }
+                if (!isset($_POST["showless"])) {
+                    if (isset($_POST["showmore"])) {
+                        $maxItems = -1;
                     }
-
-                    $_POST["showless"] = null;
-                    $_POST["showmore"] = null;
                 }
+
+                $_POST["showless"] = null;
+                $_POST["showmore"] = null;
 
                 $feedURL = "https://www.trumba.com/calendars/csuci-academic-calendar.rss";
                 $feed = simplexml_load_file($feedURL);
