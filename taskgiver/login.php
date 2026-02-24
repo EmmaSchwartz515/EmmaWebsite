644ac65c221d901da6ff9b07922a756989853f45
EmmaSchwartz <emma.schwartz515@myci.csuci.edu>
2026-02-24 09:28:23 -0800

    Add login script to login php

--- taskgiver/login.php
+++ taskgiver/login.php
@@ -87,6 +87,7 @@
             </form>
         </div>
 
+        <script src="./login.js"></script>
         <script src="https://kit.fontawesome.com/2bceb4ad9c.js" crossorigin="anonymous"></script>
     </body>
 </html>
