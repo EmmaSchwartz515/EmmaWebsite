cf1bc4287f1aeb1f38ad2e4b6024fccdd28c5149
EmmaSchwartz <emma.schwartz515@myci.csuci.edu>
2026-02-24 11:35:15 -0800

    Stylize container.php

--- rssfeed/container.php
+++ rssfeed/container.php
@@ -1,33 +1,49 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title></title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="../index.css">
+    </head>
+    <body>
+        <div class="main">
+            <?php
+                $rssUrl = "https://www.trumba.com/calendars/csuci-academic-calendar.rss";
 
-<?php
-    $rssUrl = "https://www.trumba.com/calendars/csuci-academic-calendar.rss";
+                $maxItems = 5;
 
-    $maxItems = 5;
+                if (!isset($_POST["showless"])) {
+                    if (isset($_POST["showmore"])) {
+                        $maxItems = -1;
+                    }
+                }
 
-    if (!isset($_POST["showless"])) {
-        if (isset($_POST["showmore"])) {
-            $maxItems = -1;
-        }
-    }
+                $_POST["showless"] = null;
+                $_POST["showmore"] = null;
 
-    $_POST["showless"] = null;
-    $_POST["showmore"] = null;
+                include "./rssfeed.php";
 
-    include "./rssfeed.php";
+                rssv($maxItems, $rssUrl);
 
-    rssv($maxItems, $rssUrl);
+                if ($maxItems > 0) {
+                    echo '
+                        <form action="" method="post">
+                            <input type="submit" name="showmore" value="Show more">
+                        </form>
+                    ';
+                } else {
+                    echo '
+                        <form action="" method="post">
+                            <input type="submit" name="showless" value="Show less">
+                        </form>
+                    ';
+                }
+            ?>
+        </div>
 
-    if ($maxItems > 0) {
-        echo '
-            <form action="" method="post">
-                <input type="submit" name="showmore" value="Show more">
-            </form>
-        ';
-    } else {
-        echo '
-            <form action="" method="post">
-                <input type="submit" name="showless" value="Show less">
-            </form>
-        ';
-    }
-?>
+        <script src="https://kit.fontawesome.com/2bceb4ad9c.js" crossorigin="anonymous"></script>
+    </body>
+</html>
